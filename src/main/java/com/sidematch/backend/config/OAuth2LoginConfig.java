f08ef93fe789de396f200f973090c5c62983a757
jujemu <jujemu30@gmail.com>
2024-01-15 04:46:09 +0900

    Feat: OAuth 기본값 설정
    Feature/#1 OAuth2 인증 프로세스 구현
    
    - OAuth2LoginConfig에 OAuth default 설정
    - oauth2-client 의존성 추가
    - ClientRegistrationRepository를 in-memory 방식으로 구현

---
+++ src/main/java/com/sidematch/backend/config/OAuth2LoginConfig.java
@@ -0,0 +1,41 @@
+package com.sidematch.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class OAuth2LoginConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.oauth2Login(Customizer.withDefaults());
+
+        http.authorizeHttpRequests(a -> a.anyRequest().permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(this.googleClientRegistration());
+    }
+
+    private ClientRegistration googleClientRegistration() {
+        return CommonOAuth2Provider.GOOGLE
+                .getBuilder("google")
+                .clientId("asdf")
+                .clientSecret("asdf")
+                .build();
+    }
+
+}
