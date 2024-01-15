6477311556997a07c826e73a841d5b28d8173189
jujemu <jujemu30@gmail.com>
2024-01-15 20:48:35 +0900

    Feat: application properties 암호화
    Feature/#1 OAuth2 인증 프로세스 구현
    
    - jsaypt를 이용한 설정값 암호화

---
+++ src/main/java/com/sidematch/backend/config/jasypt/JasyptConfig.java
@@ -0,0 +1,35 @@
+package com.sidematch.backend.config.jasypt;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import lombok.RequiredArgsConstructor;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@RequiredArgsConstructor
+@EnableEncryptableProperties
+@Configuration
+public class JasyptConfig {
+
+    private final Environment environment;
+
+    @Bean(name = "jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        String password = environment.getProperty("jasypt");
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(password);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+}
