3794212e1eb46c7217c1b5d76ac68046172e07c6
jujemu <jujemu30@gmail.com>
2024-01-21 12:43:43 +0900

    Feat: OAuth2UserService가 반환하는 OAuth2User 구현체 변경
    Feature/#1 OAuth2 인증 프로세스 구현
    
    - DefaultOAuth2User 에서 CustomOAuth2User 으로 수정하여 ContextHolder에 저장되는 인증 객체가 userId 값을 갖도록 수정

--- src/main/java/com/sidematch/backend/config/oauth/CustomOAuth2UserService.java
+++ src/main/java/com/sidematch/backend/config/oauth/CustomOAuth2UserService.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
@@ -27,10 +26,11 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         final OAuthAttributes attributes = OAuthAttributes.of(registrationId, oAuth2User.getAttributes());
         final User user = userService.loadUserByEmailInLogin(attributes);
 
-        return new DefaultOAuth2User(
+        return new CustomOAuth2User(
                 List.of(new SimpleGrantedAuthority(user.getRole().toString())),
                 attributes.getAttributes(),
-                attributes.getNameAttributeKey()
+                attributes.getNameAttributeKey(),
+                user.getId()
         );
     }
 }
