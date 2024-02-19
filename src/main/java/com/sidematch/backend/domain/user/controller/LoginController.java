0fbc6a51185614b6095286ca6395fced1d0805a2
jujemu <jujemu30@gmail.com>
2024-02-19 15:19:26 +0900

    Refactor:

--- src/main/java/com/sidematch/backend/domain/user/controller/LoginController.java
+++ src/main/java/com/sidematch/backend/domain/user/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.sidematch.backend.domain.user.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
